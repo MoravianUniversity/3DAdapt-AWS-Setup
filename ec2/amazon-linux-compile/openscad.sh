ff76ff62b08311dcbdfc815579d9ff97efe973cd
Jeffrey Bush <jeff@coderforlife.com>
2024-05-10 16:19:37 -0400

    Adding some notes to openscad builder

--- ec2/amazon-linux-compile/openscad.sh
+++ ec2/amazon-linux-compile/openscad.sh
@@ -65,3 +65,40 @@ mkdir -p build && cd build &&
   patchelf --set-rpath '$ORIGIN/../lib64' package/bin/openscad &&
   touch package/share/openscad/locale &&
   cd package && tar -czf ../../../openscad-"$(arch)"-linux-gnu.tar.gz --owner=0 --group=0 -- * && cd ../../..
+
+
+
+
+# Test OpenScad
+# cat >input.scad <<EOF
+# inner_diameter=41; 
+# outer_diameter=inner_diameter+15;
+# Thickness=10;
+# Handle_length=90; 
+# Handle_tip_diameter=15;
+# Screw_diam=5;
+
+# difference(){
+#     union(){
+#         hull(){
+#             cylinder(Thickness,d=outer_diameter,center=false);
+#             translate([-Handle_length,0,0])cylinder(Thickness,d=Handle_tip_diameter,center=false);
+#         }
+#         translate([inner_diameter/2,-Screw_diam*2,0])cube([Screw_diam*4,Screw_diam*4,Thickness]);
+# }
+# union(){
+#     cylinder(Thickness,d=inner_diameter,center=false);
+#     translate([inner_diameter/3,-Screw_diam/2,0])cube([3*inner_diameter,Screw_diam,Thickness]);
+#     translate([inner_diameter/2+Screw_diam*2,-Screw_diam/2,Thickness/2])rotate([90,0,0])cylinder(Screw_diam*6,d=Screw_diam,center=true);
+# }
+# }
+# EOF
+
+# cat >input.json <<EOF
+# {
+#     "parameterSets": { "vars": { "inner_diameter": 35, "Thickness": 2, "Handle_length": 100 } },
+#     "fileFormatVersion": "1"
+# }
+# EOF
+# openscad -o output.stl -p input.json -P vars input.scad
+# scp ec2-user@amazonlinux2023:output.stl .
